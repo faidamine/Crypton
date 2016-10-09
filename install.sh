92b7bfe73384aff8769f4589402375549eb932b1
Faid Md Amine <faid3210@gmail.com>
2016-10-10 00:34:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,50 +4,27 @@ echo "[ ]                         Crypton  Tool                               [ 
 echo "[ ]                  Crypton Tool Install Script                        [ ]";
 echo "[ ]====================================================================[ ]";
 echo "";
-echo "[!] Install.sh will install Crypton tool in the system [Y/n]" ; 
-read baba
-if [ $baba == "y" ] ; 
-  then
-    echo " "
-  else
-    exit
-fi
 
-echo "[!] Where Do you want to install the tool? [Ex:/usr/share/doc]:";
-read refdir
-echo "[!] Checking directories..."
-if [ -d "$refdir/Crypton" ] ;
-then
-echo "[!] A directory Panel-Tracker was found! Do you want to replace it? [Y/n]:" ; 
-read mama
-if [ $mama == "y" ] ; 
-then
- rm -R "$refdir/Crypton"
+if [ $(whoami) != "root" ];then
+    echo "You must be root to do this."
+    exit 1
+fi
+cd ..
+pip install requests
+pip install Useragent
+pip install cookielib
+pip install codecs
+pip install xml
+
+cp -R Crypton/ /opt/
+echo '#!/bin/sh' > /bin/Crypton
+echo 'python2 /opt/Crypton/Crypton.py "$@"' >> /bin/Crypton
+chmod 755 /bin/Crypton
+chmod +x /bin/Crypton
+rm -rf Crypton/
+Crypton
+if [ $? -eq 0 ]; then
+	echo "WORKED ! NOW YOU CAN USE command! : Crypton"
 else
- exit
-fi
-fi
-
- echo "[!] Installing ...";
- echo "";
- git clone https://github.com/faidamine/Crypton.git $refdir/Crypton;
- echo "#!/bin/bash 
- perl $refdir/Crypton/crypton.py" '${1+"$@"}' > crypton;
- chmod +x crypton;
- sudo cp crypton /usr/bin/;
- rm crypton;
-
-
-if [ -d "$refdir/Crypton" ] ;
-then
-echo "";
-echo "Tool istalled with success!";
-echo "";
-  echo "[ ]====================================================================[ ]";
-  echo "[ ]     All is done!! You can execute crypton by typing crypton !        [ ]"; 
-  echo "[ ]====================================================================[ ]";
-  echo "";
-else
-  echo "[!] Installation faid!! ";
-  exit
+    echo "ERROR FAIL!"
 fi
